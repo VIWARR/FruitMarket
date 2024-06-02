879c7ccaa886bf11d3da7d97ca5b509c963fce77
VIVARR <dm.pavlyuchenko@gmail.com>
2024-06-02 18:32:39 +0300

    initial commit

---
+++ src/main/java/by/VIWARR/FruitMarket/FruitMarketApplication.java
@@ -0,0 +1,13 @@
+package by.VIWARR.FruitMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitMarketApplication.class, args);
+	}
+
+}
---
+++ src/main/java/by/VIWARR/FruitMarket/Models/Company.java
@@ -0,0 +1,87 @@
+package by.VIWARR.FruitMarket.Models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "Company")
+public class Company {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "description")
+    private String description;
+
+    public Company() {}
+
+    public Company(String username, String password, String description) {
+        this.username = username;
+        this.password = password;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return Objects.equals(id, company.id) && Objects.equals(username, company.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
