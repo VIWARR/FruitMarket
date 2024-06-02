cb345bb69f14b3cda68db25ab84be75b75ea7fe8
VIVARR <dm.pavlyuchenko@gmail.com>
2024-06-02 19:06:38 +0300

    initial

---
+++ src/main/java/by/VIWARR/FruitMarket/services/ProductService.java
@@ -0,0 +1,33 @@
+package by.VIWARR.FruitMarket.services;
+
+import by.VIWARR.FruitMarket.models.Product;
+import by.VIWARR.FruitMarket.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll(Sort.by("name"));
+    }
+
+    @Transactional
+    public void save(String name) {
+        Product product = new Product(name);
+        productRepository.save(product);
+    }
+
+
+}
